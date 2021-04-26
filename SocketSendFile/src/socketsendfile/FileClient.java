ef4aa4ef0f337ce5cac9994cf61af4902826d4db
Lưu Văn Phương <82378386+luuvanphuong130322000zero@users.noreply.github.com>
2021-04-26 23:08:23 +0700

    Add files via upload

---
+++ SocketSendFile/src/socketsendfile/FileClient.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socketsendfile;
+
+/**
+ *
+ * @author hp
+ */
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class FileClient {
+
+    private Socket s;
+
+    public FileClient(String host, int port, String file) {
+        try {
+            s = new Socket(host, port);
+            sendFile(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendFile(String file) throws IOException {
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+        FileInputStream fis = new FileInputStream(file);
+        byte[] buffer = new byte[4096];
+
+        while (fis.read(buffer) > 0) {
+            dos.write(buffer);
+        }
+
+        fis.close();
+        dos.close();
+    }
+
+    public static void main(String[] args) {
+        FileClient fc = new FileClient("localhost", 1988, "D:/client.txt");
+    }
+
+}
